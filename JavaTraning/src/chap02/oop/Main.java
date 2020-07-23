b5f7ec76845d7b40c20bc87ba7f9c4531a21382b
THUANQN <thuannguyenquang02081989@gmail.com>
2020-07-23 22:05:43 +0700

    Update 230720

--- JavaTraning/src/chap02/oop/Main.java
+++ JavaTraning/src/chap02/oop/Main.java
@@ -3,6 +3,28 @@ package chap02.oop;
 public class Main {
   public static void main(String[] args) {
     
+    Student studentOne = new Student();
+    Student studentTwo = new Student();
+
+    studentOne.birthday = 1994;
+    studentOne.name = "John";
+    studentOne.code = "S001";
+
+    studentTwo.birthday = 1996;
+    studentTwo.name = "Jenny";
+    studentTwo.code = "S002";
+
+    System.out.println("Name: " + studentOne.name);
+    System.out.println("Code: " + studentOne.code);
+    System.out.println("Birthday: " + studentOne.birthday);
+
+    System.out.println("--------------------------------");
+
+    System.out.println("Name: " + studentTwo.name);
+    System.out.println("Code: " + studentTwo.code);
+    System.out.println("Birthday: " + studentTwo.birthday);
+
   }
 }
+
 
---
+++ JavaTraning/src/chap02/oop/Student.java
@@ -0,0 +1,7 @@
+package chap02.oop;
+
+public class Student {
+  public String name; 
+  public String code; 
+  public int birthday;
+}
